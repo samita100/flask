0d13882de0109e04e74b7e5da6eeae0dfc035981
samita100 <66769151+samita100@users.noreply.github.com>
2020-10-31 14:10:11 +0545

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import json
+import requests
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+  response = requests.get('https://www.proxyscan.io/api/proxy?ping=100')
+  jsons = json.loads(response.text)
+  for i in jsons:
+    ip = i["Ip"]
+    port = i["Port"]
+    t = i["Type"]
+    tf = t[0].lower()
+    final = f'{tf}://{ip}:{port}'
+    print(final)
+  return final
+
+if __name__ == '__main__':
+    app.run()
+
